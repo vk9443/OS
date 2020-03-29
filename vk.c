f776498ac52efc99174b17a2cb60a9ca3d4fcb9a
vk9443 <43479473+vk9443@users.noreply.github.com>
2020-03-29 15:13:03 +0530

    Update vk.c

--- vk.c
+++ vk.c
@@ -26,108 +26,158 @@ for process and average waiting time and turnaround time.
 
 */
 #include<stdio.h>
-#include<conio.h>
-int main() 
-{ 
-  int count,n;
-  int time=0;
-  printf("\n\nEnter the Total number of Process:\t ");                   //Taking inpute from user for Number of Proccess *vk*
-  scanf("%d",&n);                                                        //Taking inpute from user for Number of Proccess *vk*
-  int waiting_time,turn_around_time,arrival_time[n],burst_time[n],rt[n],t[n]; //Declaring of the variable name for further use in the code
-  for(count=0;count<n;count++) //For loop to get 
-  { 
-    printf("\nEnter Arrival Time Process %d :",count+1); 
-    scanf("%d",&arrival_time[count]); 
-    if(arrival_time[count] == 0)
-    {   
-    	printf("\nplease enter the arival time greater than 0\n");                                   //Checking for the condition asked in the question
-    	scanf("%d",&arrival_time[count]); 
-	}
-	printf("\nEnter Burst Time for Process %d :",count+1); 
-    scanf("%d",&burst_time[count]); 
-    rt[count]=burst_time[count]; 
-  } 
-	printf("\n\n TIME TO SHIFT FROM ONE PROCESS TO OTHER = 2\n\n");                             //To print the output
-  printf("\n\nProcess\t | Arrival Time  |  Burst Time   | Turnaround Time | Waiting Time\n\n");    // To print table
+#include<stdlib.h>
+struct process
+{
+    int process_no;
+    int p_arrival;
+    int process_burst;
 
+};
+struct result
+{
+    int final_pno;
+    int waiting_time;
+    int turnaround_time;
+};
+int main()
+{
+printf("\n\n\t\t\t\t\t*PROBLEM STATEMENT*\n");
+printf("\n\t\t3. Consider a scheduler which schedules the job by considering the arrival time of the \n\tprocesses where arrival time if given as 0 is discarded or displayed as error. The scheduler \n\timplements the shortest job first scheduling policy, but checks the queue of the processes after \n\tthe every process terminates and time taken for checking and arranging the process according \n\tto the shortest job is 2 time unit. Compute the waiting time, turnaround time and average waiting \n\ttime and turnaround time of the processes. Also compute the total time taken by the processor \n\tto compute all the jobs.");
+	printf("\n\n");
+	system ("PAUSE"); 
+	
+	system("cls");
+	struct process prc_obj[30];
+	struct result res[30];
+	int i,n,j,choice1,choice2,pos,min,pro_exec,time=0,end,k,flag,ideal_time;
+X: Y:	printf("Enter the no. of processes-");
+	scanf("%d",&n);
 
-
- int i,flag=0;
- 
-for(count=0;count<n;count++)
-{	
-int a;
-int min=100,min2=100;
-for(i=0;i<n;i++){
-	if(time!=0){
-	if(rt[i]<=min && rt[i]!=0 && arrival_time[i]<=time){
-		a=i;
-		min=rt[i];
-	}
-}
-else{
-	 if(arrival_time[i]<=min){
-		a=i;
-		min=arrival_time[i];
-	}
-}
-}
-if(time==0){
-
-for(i=0;i<n;i++){
-if (min==arrival_time[i])
-flag++;
-}
-}
-if(flag>1){
-for(i=0;i<n;i++){
-
-	if(arrival_time[i]<=min && rt[i]<=min2){
-		a=i;
-		min=arrival_time[i];
-		min2=rt[i];
-	}
-	flag=0;
-}
-}
-if(arrival_time[a]>time){
-	time=arrival_time[a]+rt[a];
-	rt[a]=0;
-	t[a]=time;
-}
-else{
-
-time=time+rt[a]	;
-	rt[a]=0;
-	t[a]=time;
-}
-}
-
-float avg_wt,avg_tat;
-float tot_wt=0,tot_tat=0;
-int a;
-
-for (count=0;count<n;count++){ 
-	if(count == 0)
+	for(i=1;i<=n;i++)
 	{
-		a=0;
+		printf("\n\t For Process P%d\n",i);
+		prc_obj[i].process_no=i;
+		printf("\t\tEnter Arrival Time:");
+		scanf("%d",&prc_obj[i].p_arrival);
+		printf("\t\tEnter Burst Time:");
+		scanf("%d",&prc_obj[i].process_burst);
+
 	}
-	else
+	printf("\n\tPress 1 to proceed to result or press 2 to re-enter the details ");
+	scanf("%d",&choice1);
+	if(choice1==2){
+		
+			system("cls");
+			goto X;
+	}time!=0;
+	for(i=1;i<=n;i++)
 	{
-		a=2;
+		if(prc_obj[i].p_arrival==0)
+        {
+            printf("\t\tProcess- %d Discared! Incorrect arrival time provided.",i);
+            for(j=i;j<n;j++)
+            {
+                prc_obj[j]=prc_obj[j+1];
+            }
+            time=prc_obj[i].p_arrival; 
+            n--;
+            i--;
+        }
+        if(prc_obj[i].p_arrival<time )
+        {
+            time=prc_obj[i].p_arrival;
+        }
 	}
-      printf("P[%d]\t|\t%d\t|\t%d\t|\t%d\t|\t%d\n",count+1,arrival_time[count],burst_time[count],t[count]-arrival_time[count],(t[count]+a)-arrival_time[count]-burst_time[count]);
-	  tot_wt = tot_wt + (t[count]+a)-arrival_time[count]-burst_time[count] ;
-	   tot_tat = tot_tat + t[count]-arrival_time[count];
-      waiting_time+=time-arrival_time[count]-burst_time[count]; 
-      turn_around_time+=time-arrival_time[count]; 
-      
-}
-avg_wt = tot_wt / n;  // To calculate average waiting time
-avg_tat = tot_tat / n; // To calculate average turn around time 
+	end = n;
+	min=0;
+	k=1;
+	while(n>0)
+	{
+	    pro_exec=prc_obj[1].process_burst;
+	    ideal_time=time+2;
+	    flag=0;
+		for(j=1;j<=n;j++)
+		{
 
-printf(" \n\nAverage waiting time : %f",avg_wt); // To print Average waiting time	
-printf("\n\nAverage turn_arounf time : %f",avg_tat); //To print Average turn around time 
+			if(pro_exec>=prc_obj[j].process_burst && prc_obj[j].p_arrival<=time)
+			{
+				pro_exec = prc_obj[j].process_burst;
+				pos=j;
+				flag=1;
+			}
+			if(ideal_time>time && ideal_time>prc_obj[j].p_arrival && prc_obj[j].p_arrival!=time)
+            {
+                ideal_time=prc_obj[j].p_arrival;
+            }
+		}
+		if(flag==1)
+        {
+            if(pos==1)
+            {
+                res[k].waiting_time=time-prc_obj[pos].p_arrival;
+                time = time + pro_exec;
+                res[k].turnaround_time=time-prc_obj[pos].p_arrival;
+            }
+            else
+            {
+                res[k].waiting_time=time-prc_obj[pos].p_arrival+2;
+                time = time + pro_exec;
+                res[k].turnaround_time=time-prc_obj[pos].p_arrival+2;
+            }
+            res[k].final_pno=prc_obj[pos].process_no;
+            for(i=pos;i<n;i++)
+            {
+               prc_obj[i]=prc_obj[i+1];
+            }
+            n--;
+            k++;
 
+        }
+        else //CPU ideal time
+        {
+            time = ideal_time;
+        }
+
+        }
+        system("cls");
+        printf("\n\n==================================================================================================================\n");
+        printf("Order of execution of processes are:\n");
+        for(i=1;i<=end;i++)
+        {
+        printf(" P%d-->",res[i].final_pno);	
+		}
+		printf("FINISH");
+		printf("\n\n==================================================================================================================\n");
+         printf("\n\n\n\t ====================================================================");
+            printf("\n\t *  PROCESS   :   WAITING TIME   :   TURNAROUND TIME");
+        for(i=1;i<=end;i++)
+        {
+            printf("\n\t *  P%d        :    %d             :         %d      ",res[i].final_pno,res[i].waiting_time,res[i].turnaround_time);
+
+        }
+printf("\n\t ====================================================================");
+
+	float sum2=0;
+float sum1=0;
+	for(i=1;i<=end;i++)
+	{
+		sum2=sum2+res[i].turnaround_time;}
+		float average_tat=sum2/end;
+		printf("\n\tAverage Turn around time is: %f",average_tat);
+		for(i=1;i<=end;i++)
+	{
+		sum1=sum1+res[i].waiting_time;}
+		float average_wt=sum1/end;
+			printf("\n\tAverage Waiting time is: %f",average_wt);
+		printf("\n\tPress 1 to calculate again or press 2 to exit: ");
+	scanf("%d",&choice1);
+	if(choice1==1){
+		
+			system("cls");
+			goto Y;
+	}
+		
+    return 0;
 }
 /* Coded by Vinod Kumar */
